8ce0a5493cc5a4cda653538e1f987cc8cda6cfe8
ahn-sj <jaesa5221@gmail.com>
2022-11-14 13:09:10 +0900

    🎉Begin: Init Setting All Entity

--- src/main/java/com/ono/omg/OmgApplication.java
+++ src/main/java/com/ono/omg/OmgApplication.java
@@ -2,7 +2,9 @@ package com.ono.omg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class OmgApplication {
 

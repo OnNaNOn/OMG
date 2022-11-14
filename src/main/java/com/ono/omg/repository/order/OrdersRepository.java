7ca38b014c96e6e7c43d836ed8735cd1d618e560
ahn-sj <jaesa5221@gmail.com>
2022-11-14 16:53:40 +0900

    Rename: OrderRepository Entity Orders to Order

--- src/main/java/com/ono/omg/repository/order/OrdersRepository.java
+++ src/main/java/com/ono/omg/repository/order/OrdersRepository.java
@@ -1,9 +1,9 @@
 package com.ono.omg.repository.order;
 
-import com.ono.omg.domain.Orders;
+import com.ono.omg.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrdersRepository extends JpaRepository<Orders, Long> {
+public interface OrdersRepository extends JpaRepository<Order, Long> {
 
 
 }

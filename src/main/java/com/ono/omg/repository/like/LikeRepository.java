0988983f6125358acb8d3e161cd01419bb2cd2a1
leeseungwoo <vugil@naver.com>
2022-11-15 00:19:07 +0900

    ⭐Feat : 좋아요 동시성 문제 해결로직 구현완료 #18

--- src/main/java/com/ono/omg/repository/like/LikeRepository.java
+++ src/main/java/com/ono/omg/repository/like/LikeRepository.java
@@ -3,10 +3,13 @@ package com.ono.omg.repository.like;
 import com.ono.omg.domain.Like;
 import com.ono.omg.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
+import javax.persistence.LockModeType;
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
     Optional<Like> findByProductAndUserid(Product product, Long id);
 }

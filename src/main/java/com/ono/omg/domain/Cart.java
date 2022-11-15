0f0ef4ff503fd6a711f090b3d8d02c172062a1c4
ahn-sj <jaesa5221@gmail.com>
2022-11-15 20:44:30 +0900

    Feat: Edit logging

--- src/main/java/com/ono/omg/domain/Cart.java
+++ src/main/java/com/ono/omg/domain/Cart.java
@@ -12,8 +12,7 @@ import javax.persistence.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Cart extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)

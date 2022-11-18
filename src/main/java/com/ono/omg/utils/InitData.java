fae5e045c194ab3ec637cf077aebc470fe5e2718
leeseungwoo <vugil@naver.com>
2022-11-18 13:20:36 +0900

    - ⭐Feat : python 더미데이터 코드생성 (일부 수정 필요)

--- src/main/java/com/ono/omg/utils/InitData.java
+++ src/main/java/com/ono/omg/utils/InitData.java
@@ -21,11 +21,11 @@ public class InitData {
 
     private final PasswordEncoder passwordEncoder;
 
-    @PostConstruct
-    public void init() {
-        for (int i = 1; i <= 10; i++) {
-            accountRepository.save(new Account(AccountType.ROLE_STANDARD, "안" + i, passwordEncoder.encode("감난다"), DeletedType.DELETE_NO));
-            productRepository.save(new Product("피카츄" + i, 1000 + i, "포켓몬", "초고속 배송", 15 + i, (long) i));
-        }
-    }
+//    @PostConstruct
+//    public void init() {
+//        for (int i = 1; i <= 10; i++) {
+//            accountRepository.save(new Account(AccountType.ROLE_STANDARD, "안" + i, passwordEncoder.encode("감난다"), DeletedType.DELETE_NO));
+//            productRepository.save(new Product("피카츄" + i, 1000 + i, "포켓몬", "초고속 배송", 15 + i, (long) i));
+//        }
+//    }
 }

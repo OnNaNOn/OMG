b4685f2a4a1aa4258f33a860cf02bd9a90065474
leeseungwoo <vugil@naver.com>
2022-11-21 19:38:58 +0900

    - ⭐Feat : scheduler 기능추가 #77

--- src/main/java/com/ono/omg/OmgApplication.java
+++ src/main/java/com/ono/omg/OmgApplication.java
@@ -3,8 +3,10 @@ package com.ono.omg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 public class OmgApplication {
 	public static void main(String[] args) {

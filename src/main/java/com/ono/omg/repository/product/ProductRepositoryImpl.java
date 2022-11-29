eee79a367ef93b56322a68d9fd8c99331c65cc9b
ahn-sj <jaesa5221@gmail.com>
2022-11-29 22:35:20 +0900

    Feat: Apply Full Text Search With Row Look Up

--- src/main/java/com/ono/omg/repository/product/ProductRepositoryImpl.java
+++ src/main/java/com/ono/omg/repository/product/ProductRepositoryImpl.java
@@ -16,8 +16,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.ono.omg.domain.QProduct.product;
@@ -109,6 +111,43 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
 
 //        JPAQuery<Product> countQuery = queryFactory.selectFrom(product);
 //        return PageableExecutionUtils.getPage(results, pageable, () -> countQuery.fetch().size());
+
+        return new PageImpl<>(results, pageable, results.size());
+    }
+
+    @Override
+    public Page<SearchResponseDto> searchProductUsedFullTextSearchAndRowLookup(SearchRequestDto requestDto, Pageable pageable) {
+
+        // 1) 커버링 인덱스로 대상 조회
+        List<Long> ids = queryFactory
+                .select(product.id)
+                .from(product)
+                .where(titleMatch(requestDto.getTitle()))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        // 1-1) 대상이 없을 경우 추가 쿼리 수행 할 필요 없이 바로 반환
+        if (CollectionUtils.isEmpty(ids)) {
+            return new PageImpl<>(new ArrayList<>(), pageable, 0);
+        }
+
+        // 2) 1의 결과로 발생한 id로 실제 select절 조회
+        List<SearchResponseDto> results = queryFactory
+                .select(new QSearchResponseDto(
+                                product.title,
+                                product.price,
+                                product.stock,
+                                product.category,
+                                product.delivery,
+                                product.sellerId,
+                                product.isDeleted,
+                                product.imgUrl
+                        )
+                )
+                .from(product)
+                .where(product.id.in(ids))
+                .fetch();
 
         return new PageImpl<>(results, pageable, results.size());
     }

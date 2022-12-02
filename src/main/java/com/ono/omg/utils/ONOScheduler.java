e11a3c243f6ee55d451127324f95105305d190da
ahn-sj <jaesa5221@gmail.com>
2022-12-02 17:41:28 +0900

    Chore: gitignore Not Contain logback-spring.xml

--- src/main/java/com/ono/omg/utils/ONOScheduler.java
+++ src/main/java/com/ono/omg/utils/ONOScheduler.java
@@ -20,7 +20,7 @@ public class ONOScheduler {
         this.productRepository = productRepository;
     }
 
-    @Scheduled(cron = "05 48 16 * * *") // 21-30일까지 02시 13분 55초에 스케줄 동작 >> cron = "55 13 02 21-30 * *"
+    @Scheduled(cron = "0 0 18 * * *") // 21-30일까지 02시 13분 55초에 스케줄 동작 >> cron = "55 13 02 21-30 * *"
     public void createEvent() {
         Product savedProduct = productRepository.save(new Product("에어팟 프로2", 5000, "이벤트 상품", "초고속 배송", 500000, 1L));
         LocalDateTime now = LocalDateTime.now();

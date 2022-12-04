74896b6075784e4ec2d39c9e1a16d4633dbe05c7
leeseungwoo <vugil@naver.com>
2022-12-04 21:11:43 +0900

    - Test : CI/CD 오류현상 해결테스트

--- src/main/java/com/ono/omg/utils/S3Config.java
+++ src/main/java/com/ono/omg/utils/S3Config.java
@@ -9,6 +9,7 @@
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 //
+
 //@Configuration
 //public class S3Config {
 //    @Value("${cloud.aws.credentials.accessKey}")
